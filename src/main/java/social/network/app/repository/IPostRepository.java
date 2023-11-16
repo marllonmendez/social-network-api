6256d45ac5624bdca4918f84144864ffed0f7786
marllonmendez <marlonmendesor@gmail.com>
2023-11-15 21:42:29 -0300

    change: readme structure

--- src/main/java/social/network/app/repository/IPostRepository.java
+++ src/main/java/social/network/app/repository/IPostRepository.java
@@ -7,9 +7,6 @@ import social.network.app.model.PostModel;
 import java.util.List;
 
 public interface IPostRepository extends JpaRepository<PostModel, Integer> {
-    List<PostModel> findByUserId(Integer userId);
-    PostModel findByIdAndUserId(Integer id, Integer idUser);
-    PostModel findByText(String text);
     @Transactional
     void deleteByUserId(Integer userId);
 }
